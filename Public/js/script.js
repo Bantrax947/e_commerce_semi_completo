1b9f336c7e6964acc345d99a8c49723dd1490466
Bantrax947 <francofabian947@gmail.com>
2024-06-08 13:08:52 -0400

    se agrego un nav al login para acceso anticipado a la página

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,11 +1,25 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+    const navSignInBtn = document.getElementById('navSignInBtn');
+    const navSignUpBtn = document.getElementById('navSignUpBtn');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+    // Eventos para los botones dentro de la animación de transición
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+
+    // Eventos para los botones de la barra de navegación
+    navSignInBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+
+    navSignUpBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
 });
