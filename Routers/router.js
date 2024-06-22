a77e779f203226fab19c591c06a9890186430e49
Bantrax947 <francofabian947@gmail.com>
2024-06-22 13:36:48 -0400

    se muestra la imagenes enel get.ejs

--- Routers/router.js
+++ Routers/router.js
@@ -2,14 +2,27 @@
 const router = express.Router();
 const authController = require('../Controllers/authController');
 const conexion = require('../DataBase/Conexion');
+const rateLimit = require('express-rate-limit');
+const fileUpload = require('express-fileupload');
+
+// Middleware de express-fileupload
+router.use(fileUpload());
 
 // Aplica el rate limiter solo a la ruta de login
-const rateLimit = require('express-rate-limit');
-
 const loginLimiter = rateLimit({
-    windowMs: 1* 60 * 1000, // 15 minutos
-    max: 2, // Limita a 5 intentos por IP en el periodo de tiempo
-    message: 'Demasiados intentos de inicio de sesión desde esta IP, por favor intente de nuevo después de 15 minutos.'
+    windowMs: 1 * 60 * 1000, // 1 minuto
+    max: 3, // Limita a 3 intentos por IP en el periodo de tiempo
+    handler: (req, res) => {
+        res.status(429).render('Client/login', {
+            alert: true,
+            alertTitle: 'Error',
+            alertMessage: 'Demasiados intentos de inicio de sesión desde esta IP, por favor intente de nuevo después de 1 minuto.',
+            alertIcon: 'error',
+            showConfirmButton: true,
+            timer: 3000,
+            ruta: 'login'
+        });
+    }
 });
 
 // Rutas públicas
@@ -21,14 +34,62 @@ router.get('/register', (req, res) => {
     res.render('Client/login', { alert: false });
 });
 
-// RUTAS DEL CRUD
+// Ruta para obtener los productos y renderizar la vista
 router.get('/get', (req, res) => {
-    conexion.query('SELECT * FROM usuario', (error, results) => {
+    conexion.query('SELECT * FROM productos', (error, results) => {
         if (error) {
-            console.log("NO FUNCIONÓ EL LLAMADO");
+            console.log("Error en la consulta de la base de datos");
             res.status(500).send("Error en la consulta de la base de datos");
         } else {
             res.render('Admin/get', { results: results });
+        }
+    });
+});
+
+// Ruta para agregar productos a la base de datos
+router.post('/add-product', (req, res) => {
+    const { productName, productPrice, productCategory } = req.body;
+    const productImage = req.files ? req.files.productImage : null;
+
+    if (!productName || !productPrice || !productCategory || !productImage) {
+        return res.status(400).send("Todos los campos son requeridos");
+    }
+
+    const imagePath = `/uploads/${productImage.name}`;
+    productImage.mv(`./public${imagePath}`, (err) => {
+        if (err) {
+            return res.status(500).send("Error al subir la imagen");
+        }
+
+        const query = 'INSERT INTO productos (titulo, imagen, precio, id_categoria) VALUES (?, ?, ?, ?)';
+        conexion.query(query, [productName, imagePath, productPrice, productCategory], (error, results) => {
+            if (error) {
+                console.error("Error al insertar el producto:", error);
+                return res.status(500).send("Error en la consulta de la base de datos");
+            }
+
+            // Obtener el producto recién insertado
+            const newProduct = {
+                id: results.insertId,
+                titulo: productName,
+                imagen: imagePath,
+                precio: productPrice,
+                id_categoria: productCategory
+            };
+
+            res.status(200).json(newProduct); // Enviar el nuevo producto en la respuesta
+        });
+    });
+});
+
+// Ruta para obtener los productos en formato JSON (API)
+router.get('/api/productos', (req, res) => {
+    conexion.query('SELECT * FROM productos', (error, results) => {
+        if (error) {
+            console.log("Error en la consulta de la base de datos");
+            res.status(500).send("Error en la consulta de la base de datos");
+        } else {
+            res.json(results);
         }
     });
 });
@@ -58,6 +119,8 @@ router.get('/carrito', (req, res) => {
 router.get('/logout', authController.logout);
 
 module.exports = router;
+ 
+
  */
 
 const express = require('express');
@@ -69,7 +132,7 @@ const rateLimit = require('express-rate-limit');
 // Aplica el rate limiter solo a la ruta de login
 const loginLimiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minuto
-    max: 3, // Limita a 2 intentos por IP en el periodo de tiempo
+    max: 3, // Limita a 3 intentos por IP en el periodo de tiempo
     handler: (req, res) => {
         res.status(429).render('Client/login', {
             alert: true,
@@ -77,7 +140,7 @@ const loginLimiter = rateLimit({
             alertMessage: 'Demasiados intentos de inicio de sesión desde esta IP, por favor intente de nuevo después de 1 minuto.',
             alertIcon: 'error',
             showConfirmButton: true,
-            timer: 100000,
+            timer: 3000,
             ruta: 'login'
         });
     }
@@ -92,21 +155,49 @@ router.get('/register', (req, res) => {
     res.render('Client/login', { alert: false });
 });
 
-// RUTAS DEL CRUD
+
+
+// Ruta para obtener los productos y renderizar la vista
 router.get('/get', (req, res) => {
     conexion.query('SELECT * FROM productos', (error, results) => {
         if (error) {
-            console.log("NO FUNCIONÓ EL LLAMADO");
+            console.log("Error en la consulta de la base de datos");
             res.status(500).send("Error en la consulta de la base de datos");
         } else {
             res.render('Admin/get', { results: results });
-        } 
-     
+        }
     });
 });
 
-//RUTA EN DONDE SE OBTIENE LOS PRODUCTOS DESDE LA BD
+// Ruta para agregar productos a la base de datos
+router.post('/add-product', (req, res) => {
+    const { productName, productPrice, productCategory, productImage } = req.body;
 
+    if (!productName || !productPrice || !productCategory || !productImage) {
+        return res.status(400).send("Todos los campos son requeridos");
+    }
+
+    const query = 'INSERT INTO productos (titulo, imagen, precio, id_categoria) VALUES (?, ?, ?, ?)';
+    conexion.query(query, [productName, productImage, productPrice, productCategory], (error, results) => {
+        if (error) {
+            console.error("Error al insertar el producto:", error);
+            return res.status(500).send("Error en la consulta de la base de datos");
+        }
+
+        // Obtener el producto recién insertado
+        const newProduct = {
+            id: results.insertId,
+            titulo: productName,
+            imagen: productImage,
+            precio: productPrice,
+            id_categoria: productCategory
+        };
+
+        res.status(200).json(newProduct); // Enviar el nuevo producto en la respuesta
+    });
+});
+
+// Ruta para obtener los productos en formato JSON (API)
 router.get('/api/productos', (req, res) => {
     conexion.query('SELECT * FROM productos', (error, results) => {
         if (error) {
@@ -118,9 +209,14 @@ router.get('/api/productos', (req, res) => {
     });
 });
 
-
 router.get('/put', (req, res) => {
     res.render('Admin/put');
+});
+
+router.get('/put/:id', (req, res) => {
+    const productId = req.params.id;
+    // Aquí puedes realizar consultas adicionales para obtener los detalles del producto con el productId
+    res.render('Admin/put', { productId: productId });
 });
 
 // Aplica el rate limiter en la ruta de login
--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 
-const app = express();
 
+const app = express();
 
 // Define el límite de rate limiting
 const loginLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 15 minutos
-    max: 2, // Limita a 5 intentos por IP en el periodo de tiempo
+    windowMs: 1 * 60 * 1000, // 1 minuto
+    max: 2, // Limita a 2 intentos por IP en el periodo de tiempo
     handler: (req, res) => {
         res.status(429).render('Client/login', {
             alert: true,
             alertTitle: 'Error',
-            alertMessage: 'Demasiados intentos de inicio de sesión desde esta IP, por favor intente de nuevo después de 15 minutos.',
+            alertMessage: 'Demasiados intentos de inicio de sesión desde esta IP, por favor intente de nuevo después de 1 minuto.',
             alertIcon: 'error',
             showConfirmButton: true,
             timer: 3000,
@@ -24,9 +24,11 @@ const loginLimiter = rateLimit({
         });
     }
 });
+
 // Configuración del motor de vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 // Middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -34,6 +36,8 @@ app.use(express.json());
 dotenv.config({ path: './env/.env' });
 app.use(cookieParser());
 app.use(morgan("dev"));
+
+
 
 // Para eliminar la cache 
 app.use(function(req, res, next) {
@@ -46,10 +50,8 @@ app.use(function(req, res, next) {
 // Llamar a la ruta
 app.use('/', require('./Routers/router'));
 
-//Conexión con el puerto
+// Conexión con el puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
-
-
